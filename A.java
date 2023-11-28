28adf147ef384805cb40611d9e7236817604f7e0
Chiupatil <150450148+Chiupatil@users.noreply.github.com>
2023-11-28 19:57:41 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,76 +1,95 @@
-package Currency_Convertor;
-
+package Employee_Payroll_System;
 import java.util.Scanner;
 
-public class A 
-{
-	 public static void main(String[] args) {
-	        System.out.println("1 Ruppe");
-	        System.out.println("2 Dollar");
-	        System.out.println("3 Euro");
-	        // take input
-	        Scanner sc = new Scanner(System.in);
-	        System. out.println("Choose the currency");
-	        int choice = sc.nextInt();
-	        System.out.println("Enter the amount");
-	        double amount = sc.nextDouble();
-	        // convert the amount
-	        switch (choice) {
-	            case 1:
-	                Ruppe_to_other(amount);
-	                break;
-	            case 2:
-	                Dollar_to_other(amount);
-	                break;
-	            case 3:
-	                Euro_to_other(amount);
-	                break;
-	            default:
-	                System.out.println("Invalid choice");
-	        }
+public class A {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter Employee Name: ");
+    String name = in.nextLine();
+    
+    System.out.println("\nEmployee Types:\n  F - Full Time\n  P - Part Time");
+    System.out.print("Enter Employee Type:");
+    String type = in.nextLine();
+    if (type.compareToIgnoreCase("F") == 0) {
+      FullTimeEmployee employee = new FullTimeEmployee(name);
+      
+      System.out.print("\nEnter Monthly Salary: ");
+      employee.setMonthlySalary(in.nextDouble());
+      
+      System.out.println(employee);
+    } else if(type.compareToIgnoreCase("P") == 0) {
+      PartTimeEmployee employee = new PartTimeEmployee(name);
+      
+      System.out.print("\nEnter Rate Per Hour: ");
+      double rate = in.nextDouble();
+      
+      System.out.print("\nEnter Number of Hours Worked: ");
+      employee.setWage(rate, in.nextInt());
+      
+      System.out.println(employee);
+    } else {
+      System.out.println("Error: Ivalid Employee Type!");
+    }
+  }
+}
 
-	    }
+abstract class Employee {
+  private String name;
+  
+  public Employee(String name) {
+    setName(name);
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+}
 
-	public static void Ruppe_to_other(double amt) {
-	    System.out.println("1 Ruppe = " + 0.013 + " Dollar");
-	    System.out.println();
+final class FullTimeEmployee extends Employee {
+  private double monthlySalary;
+  
+  public FullTimeEmployee(String name) {
+    super(name);
+  }
+  
+  public void setMonthlySalary(double salary) {
+    monthlySalary = salary;
+  }
+  
+  public double getMonthlySalary() {
+    return monthlySalary;
+  }
+  
+  public String toString() {
+    return "\nEmployee Name: " + getName()  + "\nMonthly Salary: " + monthlySalary;
+  }
+}
 
-	    System.out.println(amt+" Ruppe = " + (amt*0.013) + " Dollar");
-	    System.out.println();
-
-	    System.out.println("1 Ruppe = " + 0.012 + " Euro");
-	    System.out.println();
-	    System.out.println(amt+" Ruppe = " + (amt*0.012) + " Euro");
-	    System.out.println();
-
-	}
-	    
-
-	public static void Dollar_to_other(double amt) {
-	    System.out.println("1 Dollar = " + 79.37 + " Ruppe");
-	    System.out.println();
-	    System.out.println(amt+" Dollar = " + (amt*79.37) + " Ruppe");
-	    System.out.println();
-
-	    System.out.println("1 Dollar= " + 0.98 + " Euro");
-	    System.out.println();
-
-	    System.out.println(amt+" Dollar = " + (amt*0.98) + " Euro");
-	}
-
-	    
-
-	public static void Euro_to_other(double amt){
-	    System.out.println("1 Euro = " + 80.85 + " Ruppe");
-	    System.out.println();
-	    System.out.println(amt+" Euro = " + (amt*80.85) + " Ruppe");
-	    System.out.println();
-
-	    System.out.println("1 Euro = " + 1.02 + " Dollar");
-	    System.out.println();
-
-	    System.out.println(amt+" Euro = " + (amt*1.02) + " Dollar");
-	}
-	}
-
-
+final class PartTimeEmployee extends Employee {
+  private double ratePerHour;
+  private int hoursWorked;
+  private double wage;
+  
+  public PartTimeEmployee(String name) {
+    super(name);
+  }
+  
+  public void setWage(double rate, int hours) {
+    ratePerHour = rate;
+    hoursWorked = hours;
+    wage = ratePerHour * hoursWorked;
+  }
+  
+  public double getWage() {
+    return wage;
+  }
+  
+  public String toString() {
+    return "\nEmployee Name: " + getName()  + "\nWage: " + wage;
+  }
+}	
